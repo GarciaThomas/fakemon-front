df50aa6a9c03c6722adab2fe738264c24f4a38a2
Thomas <monsieurgt@gmail.com>
2020-05-04 12:30:03 +0200

    Debut integration springmvc

---
+++ src/main/java/controller/PlayerMechanics.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+@RequestMapping("/player")
+public class PlayerMechanics {
+	
+	@RequestMapping("/posupdate")
+	public void plyaerInfos(@RequestParam int x, @RequestParam int y) {
+		
+	}
+
+}
