a02d49c62a08c31de6bead7c1d124dffc4809931
PPJJ <ppjj.fr@gmail.com>
2020-05-06 14:35:28 +0200

    Beans de thymeleaf ajouté dans fichier WebConfig

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -7,18 +7,21 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	/**
 	 * Surcharge du resource handler pour linker les ressources statiques
-	*/
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/assets/**");
 	} 
-	
+
 	@Bean
 	public UrlBasedViewResolver viewResolver() {
 		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
@@ -27,5 +30,36 @@ public class WebConfig implements WebMvcConfigurer{
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+
+
+
+	//les 3 Beans de fonctionnement de Thymeleaf :
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();	
+		templateResolver.setPrefix("/WEB-INF/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCacheable(false);	//	ne pas faire ça en production !!!! Très problématique
+		return templateResolver;
+	}
+
+	// Comme les factories
+	@Bean
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+		viewResolver.setCharacterEncoding("UTF-8");
+		return viewResolver;
+	}
+
 
 }
